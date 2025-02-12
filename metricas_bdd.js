8cace4b15c8d2f6efe19df7ae5ef0b08d757120c
Kevin Martin <kmartinez@utt.edu.mx>
2025-02-11 19:38:40 -0600

    Fix del drop de MySQL

--- metricas_bdd.js
+++ metricas_bdd.js
@@ -1,4 +1,5 @@
-const Process = require('./Process');
+const Process = require('./utils/Process');
+const sleep = require('./utils/sleep');
 
 (async () => {
     const mysqldump = new Process("mysqldump");
@@ -12,6 +13,7 @@ const Process = require('./Process');
     const dropMysql = new Process("mysql"); 
     dropMysql.ProcessArguments.push("-uroot");
     dropMysql.ProcessArguments.push("--password=utt");
+    await sleep(3);
     dropMysql.Execute();
     dropMysql.Write("drop database Alumnos;");
     dropMysql.Write("create database Alumnos;");
@@ -19,10 +21,12 @@ const Process = require('./Process');
     dropMysql.Finish(async () => {
         console.log(`[dropMysql] Tiempo total: ${dropMysql.EndTime - dropMysql.StartTime} ms`);
 
-        const mysql = new Process("mysql");
+        const mysql = new Process("mysql", {
+            shell: true
+        });
         mysql.ProcessArguments.push("-uroot");
         mysql.ProcessArguments.push("--password=utt");
-        mysql.ProcessArguments.push("Alumnos<backup.sql");
+        mysql.ProcessArguments.push(" Alumnos < alumnos.sql");
         await mysql.ExecuteAsync(true);
         console.log(`[mysqlimport] Tiempo total: ${mysql.EndTime - mysql.StartTime} ms`);
     });
